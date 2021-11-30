abcfbbc3644ae29abec826cb6bc68a2ad5d33f0f
lioc3sar <juliocesaoguimaraes@gmail.com>
2021-11-29 22:00:12 -0300

    Third Commit

--- src/ifpe/HelloWorld.java
+++ src/ifpe/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.printf("Hello World!");
 		System.out.printf("New Hello World!");
+		System.out.printf("New World");
 
 	}
 }
